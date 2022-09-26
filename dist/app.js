84d4d99d0f6e68466d91830b21e17550915fa78a
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2022-09-26 20:06:32 +0400

    setup scripts

--- dist/app.js
+++ dist/app.js
@@ -5,3 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.use((req, res, next) => {
+    console.log(' --- request --- ', req.path, req.method);
+    next();
+    res.end();
+});
+app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
 import express from 'express';
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log(' --- request --- ', req.path, req.method);
+    next();
+    res.end();
+});
+
+app.listen(3000);
